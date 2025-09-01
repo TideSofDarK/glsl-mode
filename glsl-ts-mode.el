3b75c759322b3e2fea845222d4832bd14da00d22
TideS <tidesmain@gmail.com>
2025-09-01 12:58:37 +0500

    Tests

--- glsl-ts-mode.el
+++ glsl-ts-mode.el
@@ -168,15 +168,15 @@ Alternatively, set it to `nil' to inherit from `c-ts-mode-indent-style'."
                    ((preproc_arg) @glsl-extension-face))
      (preproc_if (identifier) @font-lock-constant-face)
      (preproc_if condition: (_ (identifier) @font-lock-constant-face))
-     (preproc_elif (identifier) @font-lock-constant-face)
-     (preproc_elif condition: (_ (identifier) @font-lock-constant-face))
+     ;; (preproc_elif (identifier) @font-lock-constant-face)
+     ;; (preproc_elif condition: (_ (identifier) @font-lock-constant-face))
      ;; (preproc_ifdef (identifier) @font-lock-constant-face)
      ;; (preproc_ifndef (identifier) @font-lock-constant-face)
      ;; (preproc_def @glsl-preprocessor-face
      ;;              name: (identifier) @font-lock-variable-name-face)
      ;; (preproc_function_def name: ((identifier) @font-lock-function-name-face))
-     (preproc_include ((string_literal) @font-lock-string-face))
-     (["#if", "#ifdef", "#ifndef", "#else", "#elif", "#endif", "#define", "include"]
+     ;; (preproc_include ((string_literal) @font-lock-string-face))
+     (["#if", "#ifdef", "#ifndef", "#else", "#elif", "#endif", "#define", "#include"]
       @glsl-preprocessor-face)
      (preproc_extension (preproc_directive) @glsl-preprocessor-face
                         extension: (identifier) @glsl-extension-face
