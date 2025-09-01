c22a7d21d2e25c231f1f3f1cb9942364a1dcc916
TideS <tidesmain@gmail.com>
2025-09-01 12:59:40 +0500

    Tests

--- glsl-ts-mode.el
+++ glsl-ts-mode.el
@@ -170,8 +170,8 @@ Alternatively, set it to `nil' to inherit from `c-ts-mode-indent-style'."
      (preproc_if condition: (_ (identifier) @font-lock-constant-face))
      (preproc_elif (identifier) @font-lock-constant-face)
      (preproc_elif condition: (_ (identifier) @font-lock-constant-face))
-     (preproc_ifdef (identifier) @font-lock-constant-face)
-     (preproc_ifndef (identifier) @font-lock-constant-face)
+     ;; (preproc_ifdef (identifier) @font-lock-constant-face)
+     ;; (preproc_ifndef (identifier) @font-lock-constant-face)
      ;; (preproc_def @glsl-preprocessor-face
      ;;              name: (identifier) @font-lock-variable-name-face)
      ;; (preproc_function_def name: ((identifier) @font-lock-function-name-face))
